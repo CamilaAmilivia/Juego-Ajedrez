ff85aee627f1c3cb0e3f2b2e071e713169db78a9
camila amilivia <camila.amiivia201654@alumnos.info.edu.ar>
2025-01-27 20:04:16 -0300

    agregue primeros get

--- main.py
+++ main.py
@@ -5,5 +5,28 @@ import chess.svg
 
 app = FastAPI()
 
-origins = ['http://localhost:7173']
+origins = ['http://localhost:5173']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+board = chess.Board()
+
+@app.get("/pieces")
+async def get_pieces():
+    return {}
+
+@app.get("/make_move/(move)")
+async def make_move(move: str):
+    return {}
+
+
+@app.get("/reset")
+async def reset():
+    return {}
 
